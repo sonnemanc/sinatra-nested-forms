e96585ea26e33c8212508c921987758c847d5829
Corban <sonnemanc@gmail.com>
2021-11-22 20:53:35 -0700

    commit tree 51c1a09072a5689af0aa56bc441fdd9816868bde

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,17 @@
 class Pirate
+    attr_accessor :name, :weight, :height
+
+    AHOY = []
+
+    def initialize(b)
+        @name = b[:name]
+        @weight = b[:weight]
+        @height = b[:height]
+        AHOY << self
+    end
+
+    def self.all
+        @@AHOY
+    end
+    
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,20 @@
 class Ship
+    attr_reader :name, :type, :booty
+
+    @@ships = []
+
+    def initialize(a)
+        @name = a[:name]
+        @type = a[:type]
+        @booty = a[:booty]
+        @@ships << self
+    end
+
+    def self.all
+        @@ships
+    end
+
+    def self.clear
+        @@ships.clear
+    end
 end
